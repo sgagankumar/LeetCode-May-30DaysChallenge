b6606402b7f11d3435861c798fd596336be1dfd3
Gagan Kumar <31625778+sgagankumar@users.noreply.github.com>
2020-05-17 13:09:55 +0530

    Added Day17

---
+++ Day17_Find_All_Anagram_in_a_String.py
@@ -0,0 +1,24 @@
+class Solution:
+	def findAnagrams(self, s: str, p: str) -> List[int]:
+		output=list()
+		p=list(p)
+		p.sort()
+		s=list(s)
+		i=0
+		plen=len(p)
+		slen=len(s)
+		while(i<(slen-plen+1)):
+			x=s[i:i+plen]
+			# print(x)
+			x.sort()
+			if(x==p):
+				output.append(i)
+				flag=True
+				while(flag and i<(slen-plen)):
+					if s[i]==s[i+plen]:
+						i+=1
+						output.append(i)
+					else:
+						flag=False
+			i+=1
+		return output
