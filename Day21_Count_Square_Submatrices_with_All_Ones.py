d08a5a1de09e29cc7566e0e945fb2015074fab5d
Gagan Kumar <31625778+sgagankumar@users.noreply.github.com>
2020-05-22 13:40:42 +0530

    Update Day21_Count_Square_Submatrices_with_All_Ones.py

--- Day21_Count_Square_Submatrices_with_All_Ones.py
+++ Day21_Count_Square_Submatrices_with_All_Ones.py
@@ -1,1 +1,15 @@
-
+class Solution:
+    def countSquares(self, matrix: List[List[int]]) -> int:
+        x=len(matrix[0])
+        y=len(matrix)
+        for i in range(y-1):
+            for j in range(x-1):
+                if matrix[i][j]!=0 and matrix[i+1][j]!=0 and matrix[i][j+1]!=0 and matrix[i+1][j+1]!=0:
+                    matrix[i+1][j+1]+=1
+                    if matrix[i][j]==matrix[i+1][j]==matrix[i][j+1]:
+                        matrix[i+1][j+1]=matrix[i][j+1]+1
+        
+        output=0
+        for i in range(y):
+            output+=sum(matrix[i])
+        return output
